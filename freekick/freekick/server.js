96521d05e5dc87bbac6518bdd104d683fce50efa
saeed <saeedasadsh@gmail.com>
2018-05-10 13:28:01 +0430

    123

--- freekick/freekick/server.js
+++ freekick/freekick/server.js
@@ -7,32 +7,6 @@ console.log('server started');
 // Avoid dead sockets by responding to the 'end' event
 var players = [];
 
-(function () {
-
-    try {
-        //var c = 0;
-        var timeout = setInterval(function () {
-
-            testRec();
-
-        }, 10000);
-    }
-    catch (e) {
-        console.log("2: " + e.message);
-    }
-})();
-
-
-
-function testRec() {
-    console.log("hi")
-    for (var i = 0; i < players.length; i++)
-    {
-
-        players[i].write("hi");
-    }
-}
-
 io.on('connection', function (socket) {
 
     console.log('client coneccted ');
@@ -44,10 +18,6 @@ io.on('connection', function (socket) {
         var sock = socket;
         var userData = { id: id, playerName: playerName, MySocket: sock };
         players[id] = userData;
-    });
-
-    socket.on('disconnectFromServer', function (data) {
-        console.log("disconnectFromServer id: " + data.id)
     });
 
     socket.on('disconnect', function (data) {
